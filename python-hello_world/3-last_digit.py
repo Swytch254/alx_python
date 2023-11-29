ebf859b145d87b7315edae113e16145ab48611fc
Nicholas Simwichi <nsimwichi@gmail.com>
2023-11-29 17:54:07 +0300

    some changes

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -4,8 +4,10 @@ last_number = str(number)[-1]
 if number > 0 and int(last_number) > 5:
     print("Last digit of {} is {} and is greater than 5".format(number, last_number))
 elif number < 0 and int(last_number) != 0:
-    print("Last digit of {} is -{} and is lesser than 6 and not 0".format(number, last_number))
-elif number < 0 and int(last_number) == 0:
+    print("Last digit of {} is -{} and is less than 6 and not 0".format(number, last_number))
+elif int(last_number) == 0:
     print("Last digit of {} is {} and is 0".format(number, last_number))
 elif number > 0 and int(last_number) < 6 and int(last_number) != 0:
     print("Last digit of {} is {} and is less than 6 and not 0".format(number, last_number))
+elif number > 0 and int(last_number) == 0:
+    print("Last digit of {} is {} and is 0".format(number, last_number))
