7fe79b26e3d39b2b5246bac97010f92fe218a267
Nicholas Simwichi <nsimwichi@gmail.com>
2023-11-30 21:36:22 +0300

    prime

--- python-functions/5-prime.py
+++ python-functions/5-prime.py
@@ -1,7 +1,8 @@
 def is_prime(number):
-    if number % 2 != 0 and number % 3 != 0:
+    if number % 2 != 0 and number % 3 != 0 and number > 0:
         return True
     elif number == 2 or number == 3:
         return True
     else:
         return False
+    
