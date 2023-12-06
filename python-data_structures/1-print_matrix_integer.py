fbf5d03a34d5d34fbdcd0241cb787e0b6e345a9d
Nicholas Simwichi <nsimwichi@gmail.com>
2023-12-06 20:41:49 +0300

    print

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -2,7 +2,10 @@ def print_matrix_integer(matrix=[[]]):
     if type(matrix) == str:
         print(matrix)
     else:
+        formatted_matrix = []
         for char in matrix:
+            formatted_row = []
             for digit in char:
-                print("{:d}".format(digit), end="")
-            print()
+                formatted_row.append("{:d}".format(digit))
+            formatted_matrix.append(" ".join(formatted_row))
+        print("\n".join(formatted_matrix))
