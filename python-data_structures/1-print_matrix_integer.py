905e21873cbb345f8ce33a403dc92c7c7c1b422a
Nicholas Simwichi <nsimwichi@gmail.com>
2023-12-06 18:11:47 +0300

    printing matrix

---
+++ python-data_structures/1-print_matrix_integer.py
@@ -0,0 +1,8 @@
+def print_matrix_integer(matrix=[[]]):
+    if type(matrix) == str:
+        print(matrix)
+    else:
+        for char in matrix:
+            for digit in char:
+                print("{} ".format(digit), end = "")
+            print()
