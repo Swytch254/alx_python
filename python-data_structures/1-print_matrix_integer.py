26dd359d2ede92973894d3f765a9d34f67ed2b25
Nicholas Simwichi <nsimwichi@gmail.com>
2023-12-06 20:31:18 +0300

    print

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -4,5 +4,5 @@ def print_matrix_integer(matrix=[[]]):
     else:
         for char in matrix:
             for digit in char:
-                print("{:d}".format(digit), end = " ")
+                print("{:d} ".format(digit), end="")
             print()
