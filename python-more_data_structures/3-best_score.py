576058d7c892664718fd5bdb2bb90d80cb9d6c8c
Nicholas Simwichi <nsimwichi@gmail.com>
2023-12-07 21:19:40 +0300

    dict

--- python-more_data_structures/3-best_score.py
+++ python-more_data_structures/3-best_score.py
@@ -1,7 +1,8 @@
 def best_score(a_dictionary):
     if type(a_dictionary) == dict:
         sorted_values = a_dictionary.values()
-        return (sorted(sorted_values, reverse=True))[0]
+        for key,value in a_dictionary.items():
+            if (sorted(sorted_values, reverse=True))[0] == value:
+                return key
     else:
         return None
-    
