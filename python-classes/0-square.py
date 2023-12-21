cac8871942764a089bd197cf4eba253f80b667a7
Nicholas Simwichi <nsimwichi@gmail.com>
2023-12-21 10:23:24 +0300

    square commented

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,4 +1,5 @@
 # class definition
-class Square:
+
+class Square:                                       #this is the class definition for Square
     def __init__(self,size=0):
         self.__size = size                          #this creates a private instance attribute that cannot be called
