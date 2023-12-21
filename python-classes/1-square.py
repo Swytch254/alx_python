5d07cf2701b3822fbc8a0f2af6b8e20730cb8265
Nicholas Simwichi <nsimwichi@gmail.com>
2023-12-21 13:52:30 +0300

    square 2

---
+++ python-classes/1-square.py
@@ -0,0 +1,27 @@
+"""
+    Square with size
+
+"""
+
+class Square:                                       
+    """
+        A class used to represent the Square class
+
+    """
+    def __init__(self,size=0):
+        """
+            Initializes the instance 
+            Parameters
+            ----------
+            size : no type, optional
+                private instance attribute, the size of the square(default is 0)
+            raises :
+                TypeError : if size is not an integer
+                ValueError : if size is less than 0
+        
+        """                        
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size  
