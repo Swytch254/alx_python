2a35912728f23c7de3d62f9e5cdbec72d655b8d2
Nicholas Simwichi <nsimwichi@gmail.com>
2023-12-21 13:53:00 +0300

    square 2

--- python-classes/0_square.py
+++
@@ -1,4 +0,0 @@
-# class definition
-class Square:
-    def __init__(self,size=0):
-        self.__size = size                          #this creates a private instance attribute that cannot be called
