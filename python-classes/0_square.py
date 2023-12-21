2ca395b3efdcafe1a61b8e79d4f2c2040ab69e3e
Nicholas Simwichi <nsimwichi@gmail.com>
2023-12-21 10:21:01 +0300

    Square2

--- python-classes/0_square.py
+++ python-classes/0_square.py
@@ -1,7 +1,3 @@
-# Write a class Square that defines a square by:
-
-#     Private instance attribute: size
-#     Instantiation with size (no type/value verification)
 # class definition
 class Square:
     def __init__(self,size=0):
