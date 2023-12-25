c15c3025111ba0409f37177f9fadd6b9e71df04b
Nicholas Simwichi <nsimwichi@gmail.com>
2023-12-25 12:52:33 +0300

    getter and setter

---
+++ python-classes/3-square.py
@@ -0,0 +1,50 @@
+"""
+    Square with size
+
+"""
+
+class Square:                                       
+    """
+        A class used to represent the Square class
+
+    """
+    def __init__(self,size=0):
+        """
+            Initializes the instance 
+            Parameters
+            ----------
+            size : no type, optional
+                private instance attribute, the size of the square(default is 0)
+            raises :
+                TypeError : if size is not an integer
+                ValueError : if size is less than 0
+        """                        
+        self.size = size
+
+
+        @property
+        def size(self):
+            """
+                A getter function for the Square class that has the size attribute
+            """
+            return self.__size
+        
+
+        @size.setter
+        def size(self, value):
+            """
+                A setter function for getting the value of size
+            """
+            if type(size) != int:
+                raise TypeError("size must be an integer")
+            elif size < 0:
+                raise ValueError("size must be >= 0")
+            self.__size = value 
+
+
+    def area(self):
+        """
+            A function to square the size
+        """
+        return self.size * self.size
+    
